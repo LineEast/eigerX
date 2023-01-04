f6ff830579f9cc02ee89d8e1d3c6798f7ef5f25f
Line East <RodjerGame@gmail.com>
2023-01-04 16:47:58 +0300

    lt ch

--- q2/Department Summary.sql
+++ q2/Department Summary.sql
@@ -4,7 +4,7 @@ create table department (
 	location text not null,
 
 	primary key (ID)
-)
+);
 
 create table employee (
 	ID serial,
@@ -15,14 +15,14 @@ create table employee (
 	foreign key (dept_id) references department (ID),
 
 	primary key (ID)
-)
+);
 
 -- собрать сводку по кол-ву сотров в каждом отделе
 -- Перечислить каждый отдел, даже если никого нет
--- Отсортировать по кол-ву сотров и по алфовитному, если кол-во сотров совпадает
+-- Отсортировать по алфовитному и по кол-ву  сотров, если кол-во сотров совпадает
 
 select department.name, count(employee.ID) as count
 from department left join employee
 on department.ID = employee.dept_id
 group by department.ID, department.name
-order by department.name, count;
+order by count desc, department.name;
